867dd02f1cb7f37b3da1e93f9808bfc528784ac6
Josh Dunn <joshdunn.uwo@gmail.com>
2017-12-07 23:49:32 -0500

    Command line arguments

--- functions/cli/qc.cli.gif.fish
+++ functions/cli/qc.cli.gif.fish
@@ -1,12 +1,32 @@
 function qc.cli.gif
-  if set -q argv[1] 
-    if set -q argv[2]
-      set file_path argv[2]
-    else
-      set file_path (string replace -r '.\w+$' '.gif' $argv[1])
+  # default arguments
+  set r 10
+  set delay 5
+
+  # update based on command line arguments
+  while set -q argv[1]
+    switch $argv[1]
+      case '-r=*'
+        set r (string match -r '[^=]*$' -- $argv[1]) 
+      case '-delay=*'
+        set delay (string match -r '[^=]*$' -- $argv[1]) 
+      case '*'
+        break
     end
 
-    eval "ffmpeg -i $argv[1] -r 10 -f gif - | gifsicle --optimize=3 --delay=5 > $file_path"
+    set -e argv[1]
+  end
+
+  if set -q argv[1] 
+    # when gif path is not set, we assume same path as movie
+    if set -q argv[2]
+      set gif_path argv[2]
+    else
+      set gif_path (string replace -r '.\w+$' '.gif' $argv[1])
+    end
+
+    # commands
+    ffmpeg -i $argv[1] -r $r -f gif - | gifsicle --optimize=3 --delay=$delay > $gif_path
   else
     echo "Missing movie path."
   end
