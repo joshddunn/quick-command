0248e3e4e7ad56376fc062437042a246b2e3a159
Josh Dunn <joshdunn.uwo@gmail.com>
2017-12-07 22:25:40 -0500

    Assume same path if no second argument for gif creation

--- functions/cli/qc.cli.gif.fish
+++ functions/cli/qc.cli.gif.fish
@@ -1,3 +1,13 @@
 function qc.cli.gif
-  eval "ffmpeg -i $argv[1] -r 10 -f gif - | gifsicle --optimize=3 --delay=5 > $argv[2]"
+  if set -q argv[1] 
+    if set -q argv[2]
+      set file_path argv[2]
+    else
+      set file_path (string replace -r '.\w+$' '.gif' $argv[1])
+    end
+
+    eval "ffmpeg -i $argv[1] -r 10 -f gif - | gifsicle --optimize=3 --delay=5 > $file_path"
+  else
+    echo "Missing movie path."
+  end
 end
