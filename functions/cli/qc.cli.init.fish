93f82ff6199ba39f9f04afc62a84dfa166efa396
Josh Dunn <joshdunn.uwo@gmail.com>
2017-12-10 17:12:42 -0500

    Refactor init function

--- functions/cli/qc.cli.init.fish
+++ functions/cli/qc.cli.init.fish
@@ -1,16 +1,15 @@
 function qc.cli.init
   if test -e quick-command.json
-    echo "A qc configuration file already exists in this directory. Please run 'qc destroy' first."
+    echo "A qc configuration file already exists in this directory. Please run 'qc cli destroy' first."
   else
     # copy the quick-command file structure
     cp $QC_PATH/templates/quick-command-init.json quick-command.json
 
     for arg in $argv
-      set -l presets (jq -r ".presets.$arg.command" $QC_PATH/templates/quick-command-presets.json) 
-      jq ".command += $presets" quick-command.json > qc.tmp.json; mv qc.tmp.json quick-command.json
-
-      set -l presets (jq -r ".presets.$arg.watch" $QC_PATH/templates/quick-command-presets.json) 
-      jq ".watch += $presets" quick-command.json > qc.tmp.json; mv qc.tmp.json quick-command.json
+      for command in "command" "watch"
+        set -l presets (jq -r ".presets.$arg.$command" $QC_PATH/templates/quick-command-presets.json) 
+        jq ".$command += $presets" quick-command.json > qc.tmp.json; mv qc.tmp.json quick-command.json
+      end
     end
 
     echo "qc has been initialized" 
