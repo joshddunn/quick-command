15e02d425e037cc7d6db9fb8aadf30f51b1f6514
Josh Dunn <joshdunn.uwo@gmail.com>
2018-01-25 23:12:05 -0500

    update install file to support linux

--- install.fish
+++ install.fish
@@ -1,5 +1,12 @@
 set -g CONFIG_PATH "$HOME/.config"
 
-brew bundle
+switch (uname)
+  case Darwin
+    brew bundle
+  case Linux
+    add-apt-repository ppa:hadret/fswatch
+    apt-get update
+    apt-get install -y jq ffmpeg fswatch
+end
 
 cp $HOME/.local/share/qc/templates/config.fish $CONFIG_PATH/fish/conf.d/qc.fish
